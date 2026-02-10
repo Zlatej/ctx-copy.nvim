b295580ac92ce738d31e1af68f84873ed618f3ae
Matej Skalicky <matej.skalickyy@gmail.com>
2026-02-10 22:50:24 +0100

    config fix

--- lua/ctx-copy/config.lua
+++ lua/ctx-copy/config.lua
@@ -1,5 +1,11 @@
 local M = {}
 
+---@class CtxCopyConfig
+---@field keymap table
+---@field prefixes string[]
+---@field remove_indent boolean
+
+---@type CtxCopyConfig
 M.defaults = {
 	keymap = {
 		cp_context = "<leader>cc",
@@ -11,6 +17,9 @@ M.defaults = {
 	remove_indent = true,
 }
 
+---@type CtxCopyConfig
+M.options = {}
+
 function M.setup(custom_opts)
 	custom_opts = custom_opts or {}
 	if custom_opts.prefix and custom_opts.prefix ~= "" then
@@ -18,10 +27,11 @@ function M.setup(custom_opts)
 			custom_opts.prefixes = {}
 		end
 		table.insert(custom_opts.prefixes, 1, custom_opts.prefix)
-		vim.notify("copy.nvim: 'prefix' option is deprecated, use 'prefixes' array instead", vim.log.levels.WARN)
+		vim.notify("ctx-copy.nvim: 'prefix' option is deprecated, use 'prefixes' array instead", vim.log.levels.WARN)
 		custom_opts.prefix = nil
 	end
-	return vim.tbl_deep_extend("force", M.defaults, custom_opts)
+	M.options = vim.tbl_deep_extend("force", M.defaults, custom_opts)
+	return M.options
 end
 
 return M
